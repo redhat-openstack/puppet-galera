3b0a3c99419c3407e5d880213d62159c42713668
Ryan O'Hara <rohara@redhat.com>
2014-05-12 12:46:41 -0500

    Remove unnecessary dup in wsrep_options

--- lib/puppet/parser/functions/wsrep_options.rb
+++ lib/puppet/parser/functions/wsrep_options.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:wsrep_options, :type => :rvalue) do |args|
-    opts = args[0].dup
+    opts = args[0]
     opts.delete_if {|key, val| val.equal? :undef}
     opts.sort.map do |k,v|
       String(k) + "=" + String(v)
